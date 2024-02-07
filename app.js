85abdc9ea9da05fcb62310f08a645dd524a1ff42
unknown <shreeshbadiger12@outlook.com>
2024-02-07 11:37:18 +0530

    fix

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 //serving the static files
-app.use(express.static("./public"));
+app.use(express.static(path.join(__dirname, "public")));
 
 
 //handle session cookie
